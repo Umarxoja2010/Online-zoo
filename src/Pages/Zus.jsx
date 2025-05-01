ff97dae1637855d8edf7a977c7c3ff9a842091a8
Umarxoja2010 <umarxojamamarasulov@gmail.com>
2025-05-01 10:24:06 +0500

    zooos

--- src/Pages/Zus.jsx
+++ src/Pages/Zus.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Pandas from '../Components/Pandas'
-import Eagles from '../Components/Eagles'
 
 function Zus() {
   return (
  <main>
     <Pandas/>
-    <Eagles/>
  </main>
   )
 }
